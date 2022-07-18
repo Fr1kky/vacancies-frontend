74ee61795908b8e208a37018ae9dc6e833231f0a
Fr1kky <tso4343@gmail.com>
2022-07-18 20:28:17 +0300

    Initial commit with usable API request

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+/////////////// Request to API on btn click
+// const btnGDF = document.querySelector('#btnGDF');
+
+// btnGDF.addEventListener('click', async () => {
+//     //  fetch('http://127.0.0.1:5000/v1/get_rate')
+//     //     .then((response) => {
+//     //         return response.json();
+//     //     })
+//     //     .then((data) => {
+//     //         console.log('GET response:');
+//     //         console.log(data.rate); 
+//     //     });
+
+//     let response = await fetch('http://127.0.0.1:5000/v1/get_rate');
+//     let data = await response.json();
+//     console.log(data.rate);
+
+//     const textOnPage = document.createElement('p');
+//     textOnPage.innerText = data.rate;
+//     root.appendChild(textOnPage);
+// });
+
+
+/////////////// Request to API on DOMContentLoad
+const root = document.querySelector('.root');
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let response = await fetch('http://127.0.0.1:5000/v1/get_rate');
+    let data = await response.json();
+    console.log(data.rate);
+
+    const textOnPage = document.createElement('p');
+    textOnPage.innerText = data.rate;
+    root.appendChild(textOnPage);
+});
