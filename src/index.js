54ab3ba3b4e91ccb269568d48d25fcf79fb4514b
Fr1kky <tso4343@gmail.com>
2022-08-02 19:10:47 +0300

    Modified index.js for work with functions in separate files

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import "./scss/main.scss";
-/////////////// Request to API on btn click
-// const btnGDF = document.querySelector('#btnGDF');
-
-// btnGDF.addEventListener('click', async () => {
-//     //  fetch('http://127.0.0.1:5000/v1/get_rate')
-//     //     .then((response) => {
-//     //         return response.json();
-//     //     })
-//     //     .then((data) => {
-//     //         console.log('GET response:');
-//     //         console.log(data.rate); 
-//     //     });
-
-//     let response = await fetch('http://127.0.0.1:5000/v1/get_rate');
-//     let data = await response.json();
-//     console.log(data.rate);
-
-//     const textOnPage = document.createElement('p');
-//     textOnPage.innerText = data.rate;
-//     root.appendChild(textOnPage);
-// });
-
+import {postDataFromAPI} from './js/postDataFromApi'
 
 /////////////// Request to API on DOMContentLoad
-const root = document.querySelector('.vacancies-wrapper');
-
-document.addEventListener("DOMContentLoaded", async () => {
-    let response = await fetch('http://127.0.0.1:5000/v1/get_rate');
-    let data = await response.json();
-    console.log(data.rate);
-
-    const textOnPage = document.createElement('p');
-    textOnPage.innerText = data.rate;
-    root.appendChild(textOnPage);
+document.addEventListener("DOMContentLoaded",  () => {
+    postDataFromAPI();
+    postDataFromAPI().then(data => {
+        console.log(data);
+    })
 });
+
